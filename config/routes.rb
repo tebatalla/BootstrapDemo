27b0aafd670af20f80c94b2dda587f6297a91ca7
Tomas Batalla <tebatalla@gmail.com>
2015-03-14 17:15:07 -0700

    Add static pages controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ AuthDemo::Application.routes.draw do
   resource :user, only: [:create, :new, :show] do
     resource :counter, only: [:update]
   end
+  get '/home', to: 'static_pages#home', as: 'home'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/about', to: 'static_pages#about', as: 'about'
 end
